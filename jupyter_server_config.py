3b4b93300100c362d977c75d6006ce0f63105c52
maskurniawan <57574729+tolecilik@users.noreply.github.com>
2025-08-19 17:42:45 +0700

    Update jupyter_server_config.py

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -1,17 +1,7 @@
 c = get_config()  # noqa
 
-# Listen di semua IP
-c.ServerApp.ip = "0.0.0.0"
+c.ServerApp.ip = "0.0.0.0"  # listen on all IPs
+c.ServerApp.allow_origin = "*"  # allow access from anywhere
 
-# Allow access dari mana saja
-c.ServerApp.allow_origin = "*"
 
-# Nonaktifkan token/password supaya platform bisa akses
-c.ServerApp.token = ""
-c.ServerApp.password = ""
-
-# Notebook root directory
-c.ServerApp.notebook_dir = "/app"
-
-# Aktifkan collaborative mode jika ingin
 c.LabApp.collaborative = True
